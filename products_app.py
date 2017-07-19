ff1c3d302ce00fd944f6d11db5e929a4f14e988f
Jason Scharf <jasonscharf@Jasons-MacBook-Air.local>
2017-07-19 18:46:10 -0400

    First step

---
+++ products_app.py
@@ -0,0 +1,16 @@
+menu = """
+    Hi.
+
+    Welcome to the products app.
+
+    There are 100 products.
+
+    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
+
+    Please choose an operation:
+
+"""
+
+chosen_operation = input(menu)
+
+print(chosen_operation)
